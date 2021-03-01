1303f2527fcd454437e18c22666c72822ad0eef5
Alexander Dahlin <alexander.dahlins@gmail.com>
2021-03-01 09:35:40 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,9 @@
 project "ImGui"
-    kind "StaticLib"
+    kind 	 "StaticLib"
     language "C++"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir 	("bin/" .. outputdir .. "/%{prj.name}")
+    objdir 	 	("bin-int/" .. outputdir .. "/%{prj.name}")
 
     files
     {
@@ -11,16 +11,17 @@ project "ImGui"
         "imgui.h",
         "imgui.cpp",
         "imgui_draw.cpp",
+        "imgui_demo.cpp"
         "imgui_internal.h",
+		"imgui_tables.cpp",
         "imgui_widgets.cpp",
         "imstb_rectpack.h",
         "imstb_textedit.h",
         "imstb_truetype.h",
-        "imgui_demo.cpp"
     }
     
     filter "system:windows"
-        systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
+        systemversion 	"latest"
+        cppdialect 		"C++17"
+        staticruntime 	"On"
 	filter {}
